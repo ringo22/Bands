40000757f318a3d4b790f10be4d09fce09e74b59
Rodrigo <roraya22@gmail.com>
2011-07-18 14:24:15 -0500

    Rails 3 proyect Bands

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+gem "mysql"
+
+gem "mysql2"
+
+gem "devise", "1.1.pre4"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bands::Application.config.secret_token = 'b64f86d002f75ffb9c64b9ff096a87b33a0f062ff79b8692dd2c79b771fab0ef6f329dbd18f6aecbdf2f266d67ff08e0146b1d2b9c562d859316e26fd087b24d'
